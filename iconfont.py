453be97d1206f137df7cdf875afbb1eeed0be907
akaziki <akaziki_x@163.com>
2019-10-25 02:10:46 +0800

    添加下载项目的方法

--- iconfont.py
+++ iconfont.py
@@ -2,13 +2,17 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from time import sleep
 from githubCode import GetGithubCode
+import os
+import urllib.request
 
 class Iconfont:
   def __init__ (self):
     self.driver = webdriver.Chrome()
     self.driver.implicitly_wait(10)
+  # 微博登录 todo
   def weiboLogin (self):
     self.driver.get('https://api.weibo.com/oauth2/authorize?client_id=1283124999&redirect_uri=https%3A%2F%2Fwww.iconfont.cn%2Fapi%2Flogin%2Fweibo%2Fcallback&response_type=code&scope=users_show&state=plus-login#weibo_redirect')
+  # github登录
   def githubLogin (self):
     self.driver.get('https://www.iconfont.cn/api/login/github')
     login_name = self.driver.find_element_by_id('login_field')
@@ -19,11 +23,15 @@ class Iconfont:
     login_submit.click()
     sleep(5)
     self.checkLogin()
+  # 判断登录是否需要邮箱验证
   def checkLogin(self):
     url = self.driver.current_url
     print('current_url', url)
     if (url == 'https://github.com/sessions/verified-device'):
       self.setVerified()
+    else:
+      # self.toHome()
+      self.toProject()
   # 输出邮箱提供的验证码
   def setVerified (self):
     code = GetGithubCode().getCode()
@@ -32,11 +40,30 @@ class Iconfont:
     verifiedSubmit = self.driver.find_element_by_class_name('btn-primary')
     verifiedInput.send_keys(code)
     verifiedSubmit.click()
+  # 跳转至首页
   def toHome (self):
     self.driver.get('https://www.iconfont.cn')
-  #def getProject(self):
-  
+  # 跳转至项目页面
+  def toProject (self):
+    self.driver.get('https://www.iconfont.cn/manage/index?manage_type=myproject')
+  # 下载项目
+  def downLoadProject(self):
+    downloadUrl = self.driver.find_element_by_partial_link_text('下载至本地')
+    print('downloadUrl', downloadUrl)
+    # 该目录下创建一个文件夹iconfont，判断没有文件夹就创建
+    if not os.path.exists('iconfont'):
+      os.mkdir('logoImg')
+    # 跳到文件夹
+    os.chdir('logoImg')
+    try:
+      urllib.request.urlretrieve(downloadUrl, 'iconfont')
+    except:
+      print('404:%s' % url)
+    else:
+      print('已下载:%s' % url)
+    os.chdir('..')
 iconfont = Iconfont()
 # iconfont.weiboLogin()
 iconfont.githubLogin()
+iconfont.downLoadProject()
 # iconfont.toHome()
